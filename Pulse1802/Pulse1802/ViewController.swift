49b3f615385478643bab4fe3ad77910b9d55fcce
manjunadhb <manjunadhb@gmail.com>
2018-08-23 18:33:39 +0530

    created new project and everybody use this project only

---
+++ Pulse1802/Pulse1802/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pulse1802
+//
+//  Created by Manjunadh Bommisetty on 23/08/18.
+//  Copyright © 2018 Manjunadh Bommisetty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
